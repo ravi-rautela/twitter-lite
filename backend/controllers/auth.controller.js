b1e91c125d1c1d88578024f4ad0088134b9b6e0c
Ravi Rautela <ranvi44rautela@gmail.com>
2024-07-05 11:20:05 +0530

    we done with bacend setups and mongodb connections.

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+// ((******* SIGNUP ********))
+export const signup = async (req, res) => {
+
+    res.json({ data: "Your hit the signup end point" })
+}
+
+// ((*******Login*********))
+export const login = async (req, res) => {
+
+    res.json({ data: "Your hit the login end point" })
+
+}
+// ((***** Logout *****))
+export const logout = async (req, res) => {
+    res.json({ data: "Your hit the logout end point" })
+}
